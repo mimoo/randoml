16c5691cf50e9e48018fb65b68c1d28dce665666
David Wong <davidwong.crypto@gmail.com>
2021-07-13 17:39:32 +0200

    added CI

--- lib/rand/rand.ml
+++ lib/rand/rand.ml
@@ -1,8 +1,8 @@
-external rand_bytes: bytes -> unit = "rand_bytes"
+external rand_bytes : bytes -> unit = "rand_bytes"
 
-external rand_int32: unit -> int32 = "rand_int32"
+external rand_int32 : unit -> int32 = "rand_int32"
 
-external rand_int64: unit -> int64 = "rand_int64"
+external rand_int64 : unit -> int64 = "rand_int64"
 
 external rand_int32_range : int32 -> int32 -> int32 = "rand_int32_range"
 
